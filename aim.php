1db5df1ee12776df608f837bfa02f5226ad20819
veermetri05 <37113369+veermetri05@users.noreply.github.com>
2018-04-23 16:12:11 +0530

    Change

---
+++ aim.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'header.php';
+?>
+<style type="text/css">
+body {
+  background-image: url("background9.jpg") ;
+  position: relative;
+  opacity: 5;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-8" align="center">
+  <p>
+    <span style="font-size:50px; background-color:black; color:white; opacity:0.7;">Our Aim</span><br><hr>
+  </p>
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+<div class="col-sm-8" aling="center" style="background-color:black; color:white; opacity:0.7;">
+  <p>
+    We are working to reach our aim for making 1000 website. Our target will reach when people like you will make a website with us. We donate our money mostly in open source App/WebBuilder/OS.<br>
+    We are working on a app which creates website within minutes. We have decided to make the app free and useful to everyone around the internet.<br>
+  </p>
+  <hr>
+  <br>
+  <p>
+    Our company is working around for 2 years and have found that the costs of website making are more on internet small business are not able to afford it. So we make powerful website with ery less amount of money. We might be back in anything then please tell us using the feedback form and remember to mention your email address in the message. Your feedback will always help us all for our development and a good future. <br>
+
+  </p>
+</div>
+<div class="col-sm-2">
+  &nbsp;
+</div>
+
+<?php
+include_once 'footer.php';
+ ?>
