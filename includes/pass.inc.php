c7926861ed6366c048b499cb77b1c4376d0ca378
veermetri05 <37113369+veermetri05@users.noreply.github.com>
2018-04-24 18:28:10 +0530

    Updated the alert of the password
    I have updated success fully the password system of change password.

--- includes/pass.inc.php
+++ includes/pass.inc.php
@@ -19,33 +19,33 @@ if (isset($_POST['submit'])) {
 	exit();
 	}
 
-if ($newpwd === $repwd) {
+
   $hashedPwd = password_hash($newpwd, PASSWORD_DEFAULT);
 
   $sql = "SELECT * FROM users WHERE user_uid='$uid'";
   $result = mysqli_query($conn, $sql);
   $resultCheck = mysqli_num_rows($result);
   if ($resultCheck < 1) {
-    $_SESSION['error'] = "yes" ;
+
   } else {
     if ($row = mysqli_fetch_assoc($result)){
 
       $HashedPwdCheck = password_verify($oldpwd, $row['user_pwd']);
-
+      if ($newpwd === $repwd) {
       if ($HashedPwdCheck == true) {
         $sql = "UPDATE users SET user_pwd = '$hashedPwd' WHERE user_uid = '$uid';";
         mysqli_query($conn, $sql);
         $_SESSION['passerror'] = "done";
         header("Location: ../password.php?password=done");
       } elseif ($HashedPwdCheck == false) {
-              $_SESSION['passerror'] = "wrongold";
+            $_SESSION['passerror'] = "wrongold";
+            header("Location: ../password.php?password=wrongold");
       }
-}
-
+} else {
+  $_SESSION['passerror'] = "notverify" ;
+  header("Location: ../password.php?password=noverify");
     }
-  } else {
-    $_SESSION['passerror'] = "notverify" ;
-    header("Location: ../password.php?password=noverify");
+  }
   }
 }
  ?>
--- password.php
+++ password.php
@@ -4,6 +4,40 @@ include_once 'header.php';
 
 <?php
 if (isset($_SESSION['u_id'])) {
+  if (isset($_SESSION['passerror'])) {
+    if ($_SESSION['passerror'] == "done") {
+      echo '<div class="alert alert-success">
+        <strong>Success!</strong>
+         Password Changed
+      </div>
+      ' ;
+      $_SESSION['passerror'] = "used" ;
+    } elseif ($_SESSION['passerror'] == "wrongold") {
+      echo '<div class="alert alert-danger">
+        <strong>Alert!</strong>
+        Wrong Old Password
+      </div>
+      ';
+      $_SESSION['passerror'] = "used";
+    } elseif ($_SESSION['passerror'] == "notverify" ) {
+      echo '<div class="alert alert-danger">
+        <strong>Alert!</strong>
+        Wrong re-typed password
+      </div>
+      ';
+
+      $_SESSION['passerror'] = "used";
+    } elseif ($_SESSION['passerror'] == "empty") {
+      echo '<div class="alert alert-danger">
+        <strong>Error!</strong>
+        Fill all the fields.
+      </div>
+      ';
+
+      $_SESSION['passerror'] = "used";
+    }
+    echo '</div></div> ';
+  }
   echo '
   <div class="col-sm-12" align="center">
     <form action="includes/pass.inc.php" method="POST">
