b24a832a09a22c8f1f6a58959c147e2d08240981
veermetri05 <37113369+veermetri05@users.noreply.github.com>
2018-04-25 16:41:05 +0530

    Update
    I have updated the future

--- future.php
+++ future.php
@@ -13,7 +13,7 @@ body {
 }
 
 </style>
-<div class="col-sm-12" style="height:200px;" align="center">
+<div class="jumbotron" style="background-color:#fff; opacity:0.7;" align="center">
   <p>
     Future
   </p>
@@ -22,7 +22,7 @@ body {
   <div class="col-sm-2">
     &nbsp;
   </div>
-  <div class="col-sm-8">
+  <div class="col-sm-8" style="background-color:#ffffff; opacity:0.7;">
     <p>
       Our aim is to make a free and open source project from which people can earn lots of money. We have a very good experience in making the websites. We will soon launch our product <b>VM</b> Web Development tool. Which will work online and will be fully built on HTML and CSS. We will make this project on <a href="https://github.com/VM-Inc">GitHub</a>. We will fix all the bugs as early as possible by us. If you want to make a website for your company, business, shop, hotel, work, etc. we are always ready for it just contact us or mail us or leave a feed back so we will get connected to you.
     </p>
