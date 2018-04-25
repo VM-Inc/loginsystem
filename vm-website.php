3c6dd2a71fc6f0a9623b84386c147c39ef95bc7e
veermetri05 <37113369+veermetri05@users.noreply.github.com>
2018-04-25 16:28:52 +0530

    Update
    Update

--- vm-website.php
+++ vm-website.php
@@ -1,12 +1,6 @@
 <?php
 include_once 'header.php';
 ?>
-<html>
-    <head>
-        <meta http-equiv="refresh" content="0; URL='http://veermetri.rf.gd/mail/index.php'" />
-    </head>
-    <body>
 
-    </body>
-    <?php include_once 'footer.php';
-    ?>
+<?php include_once 'footer.php';
+?>
