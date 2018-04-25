1665042e71a169fe53cd34dbb7a1f297213ea051
veermetri05 <37113369+veermetri05@users.noreply.github.com>
2018-04-25 16:37:44 +0530

    Update
    I have updated the future page.

--- future.php
+++ future.php
@@ -13,44 +13,22 @@ body {
 }
 
 </style>
-<div class="col-sm-2">
-  &nbsp;
-</div>
-<div class="col-sm-8" align="center">
+<div class="col-sm-12" style="height:200px;" align="center">
   <p>
-    Hello
+    Future
   </p>
 </div>
-<div class="row">
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="background7.jpg">
-        <img src="background7.jpg" alt="Lights" style="width:100%">
-        <div class="caption">
-          <p>Competition Organizing System</p>
-        </div>
-      </a>
-    </div>
+<div class="col-sm-12">
+  <div class="col-sm-2">
+    &nbsp;
   </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="background4.jpg">
-        <img src="background4.jpg" alt="Nature" style="width:100%">
-        <div class="caption">
-          <p>School Attendance System</p>
-        </div>
-      </a>
-    </div>
+  <div class="col-sm-8">
+    <p>
+      Our aim is to make a free and open source project from which people can earn lots of money. We have a very good experience in making the websites. We will soon launch our product <b>VM</b> Web Development tool. Which will work online and will be fully built on HTML and CSS. We will make this project on <a href="https://github.com/VM-Inc">GitHub</a>. We will fix all the bugs as early as possible by us. If you want to make a website for your company, business, shop, hotel, work, etc. we are always ready for it just contact us or mail us or leave a feed back so we will get connected to you.
+    </p>
   </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="background5.jpg">
-        <img src="background5.jpg" alt="Fjords" style="width:100%">
-        <div class="caption">
-          <p>Website</p>
-        </div>
-      </a>
-    </div>
+  <div class="col-sm-2">
+    &nbsp;
   </div>
 </div>
 <?php
